80d353fe2b1f359a0c5680bb09a94df3438036e9
mastermindsintl <54694501+mastermindsintl@users.noreply.github.com>
2020-05-29 13:55:02 -0400

    Add files via upload

---
+++ onslaught_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+	exclusive				= true,
+	victory_condition	= true,
+	always_on			= false,
+	title						= "Onslaught",
+	win_message 		= "You Survived? How?",
+	lose_message 	= "You did not survive the Necron Onslaught!",
+	description			= "Players versus endless waves of Orks!"
+}
