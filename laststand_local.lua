55e9d84a051ae6cf7307df98f2fa7d5626841679
mastermindsintl <54694501+mastermindsintl@users.noreply.github.com>
2020-07-01 08:44:25 -0400

    Add files via upload

---
+++ laststand_local.lua
@@ -0,0 +1,10 @@
+Localization = 
+{
+	exclusive				= true,
+	victory_condition	= true,
+	always_on			= false,
+	title						= "Last Stand",
+	win_message 		= "You Survived? How?",
+	lose_message 	= "You did not survive!",
+	description			= "Players verses the endless hordes of Chaos!"
+}
